d97647c76b95f19efab32688e7a33e0acfa58c0f
zxcv5052 <40011815+zxcv5052@users.noreply.github.com>
2020-11-09 19:04:48 +0900

    Delete user-query.js

--- LeagueC-API/src/queries/user-query.js
+++
@@ -1,3 +0,0 @@
-exports.saveUser = "INSERT INTO user(email, password, name) VALUES(?,?,?)";
-exports.getUserByEmail = "SELECT * FROM user WHERE email = ?";
-exports.getUserByName = "SELECT * FROM user WHERE name = ?";
